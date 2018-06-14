a79d0106f276d584deef974680a09bd671a5880c
Cristian Custodio <cristian325@gmail.com>
2018-06-14 16:53:05 -0400

    Added Clock and Additional logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,85 @@
 $( document ).ready(function() {
 
-    
-    //Record the answer selected for question number1
+        stopwatch.start();
+
+    //Record the answer selected for questions and stored in variables
 
     //Question 1
     $('#questionOneForm input').on('change', function() {
-         answer1 = $('input[name=question1]:checked', '#questionOneForm').val();
+         answer1 = parseInt($('input[name=question1]:checked', '#questionOneForm').val());
+         if (answer1 === 3) {
+             questionsRight++;
+             unansweredQuestions--;
+             
+         } else {
+             questionsWrong++;
+             unansweredQuestions--;
+         }
+
+         $('#questionOneForm input').attr('disabled',true);
+
+         
      });
 
     //Question 2
-    $('#questionOneForm input').on('change', function() {
-        answer2 = $('input[name=question1]:checked', '#questionOneForm').val();
+    $('#questionTwoForm input').on('change', function() {
+        answer2 = parseInt($('input[name=question2]:checked', '#questionTwoForm').val());
+        if (answer2 === 1) {
+            questionsRight++;
+            unansweredQuestions--;
+            
+        } else {
+            questionsWrong++;
+            unansweredQuestions--;
+        }
+
+        $('#questionTwoForm input').attr('disabled',true);
+       
     });
 
     //Question 3
-    $('#questionOneForm input').on('change', function() {
-        answer3 = $('input[name=question1]:checked', '#questionOneForm').val();
+    $('#questionThreeForm input').on('change', function() {
+        answer3 = parseInt($('input[name=question3]:checked', '#questionThreeForm').val());
+        if (answer3 === 2) {
+            questionsRight++;
+            unansweredQuestions--;
+        } else {
+            questionsWrong++;
+            unansweredQuestions--;
+        }
+        $('#questionThreeForm input').attr('disabled',true);
     });
 
     //Question 4
-    $('#questionOneForm input').on('change', function() {
-        answer4 = $('input[name=question1]:checked', '#questionOneForm').val();
+    $('#questionFourForm input').on('change', function() {
+        answer4 = parseInt($('input[name=question4]:checked', '#questionFourForm').val());
+        if (answer4 === 4) {
+            questionsRight++;
+            unansweredQuestions--;
+        } else {
+            questionsWrong++;
+            unansweredQuestions--;
+        }
+        $('#questionFourForm input').attr('disabled',true);
     });
-
-
-
-
     console.log( "Let the console being!" );
-
-    if(quizPage === true) {
-        stopwatch.start();
-    } else {
-
-        //Perform question calculations
-        
-        
-
-    }
-
-    
 
 });
 
+//Answer html
+
+var answerHTML = "<Div id='Game-Body'>" + "<h1>Quiz Results</h1>" + "<Div class='Quiz-Results' id='Correct-Answers'>Correct Answers 0</Div>" + "<Div class='Quiz-Results' id='Incorrect-Answers'>Incorrect Answers: 0</Div>" + "<Div class='Quiz-Results' id='Unanswered-Questions'>Unanswered: 0</Div>"
+
 //Quiz Answers Bank
-var answer1;
-var answer2;
-var answer3;
-var answer4;
+var answer1 = 0;
+var answer2 = 0;
+var answer3 = 0;
+var answer4 = 0;
 
 //Quiz Counters
 var questionsRight = 0;
 var questionsWrong = 0;
-var unansweredQuestions = 0;
+var unansweredQuestions = 4;
 
 //Navigation Counter
 var quizPage = true;
@@ -91,8 +119,12 @@ var stopwatch = {
 
         //Perform actions once stopwatch reaches 0
             stopwatch.stop();
+            $("#Game-Body").html(answerHTML);
+            //Post quiz results to QuizResults HTML
+            $("#Correct-Answers").html("Correct Answers: " + questionsRight);
+            $("#Incorrect-Answers").html("Incorrect Answers: " + questionsWrong);
+            $("#Unanswered-Questions").html("Unanswered Questions: " + unansweredQuestions);
             quizPage = false;
-            window.location.replace("quizResults.html");
             //$("#Game-Body").html(quizResults.html);
       }
   
@@ -123,5 +155,5 @@ var stopwatch = {
   };
 
 
-//alert($('input[name=question1]:checked', '#questionOneForm').val()); 
+
 
